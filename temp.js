e79dd642cda34b279540f0d1856b3ce217e12f1c
vismaydeeps <chinavismay25@gmail.com>
2024-11-20 17:57:20 +0530

    added link

---
+++ temp.js
@@ -0,0 +1,16 @@
+db.products.insertMany(
+    [
+    {
+        "name": "Product5",
+        "description":"This is the desc of prod5",
+        "price":30,
+        "url":"https://picsum.photos/id/5/200/200"
+    },
+    {
+        "name": "Product6",
+        "description":"This is the desc of prod6",
+        "price":90,
+        "url":"https://picsum.photos/id/6/200/200"
+    },
+]
+);
